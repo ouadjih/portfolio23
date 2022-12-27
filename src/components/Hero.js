7e8f5230eb28a4775d74dee2afd0a89ccc1fc9c6
ouadjih <47463757+ouadjih@users.noreply.github.com>
2022-12-27 23:11:30 +0100

    uploaded my resume

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ export const Hero = () => {
     <div className="hero">
       <h1>Hello, my name is Ouadjih.</h1>
       <p class="subtitle">Welcome to my website!</p>
-      <a className="button-74" target="_blank"  href="./Boudraa_resume.pdf">Resume</a>
+      <a className="button-74" target="_blank" href="./ouadjihboudraa.pdf">Resume</a>
     </div>
     </>
   );
