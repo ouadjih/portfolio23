3db241efa99cdfcf39fe25da86bb789a319ad955
ouadjih <47463757+ouadjih@users.noreply.github.com>
2022-12-27 17:09:02 +0100

    added projects and link to every project

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -6,6 +6,8 @@ import Loader from "./Loader";
 export const Grid = () => {
   const [filtredItems, setFiltredItems] = useState(Projects);
   const [loading, setLoading] = useState(true);
+
+
   setTimeout(()=>setLoading(false), 1000);
  
   function onChange(e) {
@@ -22,9 +24,8 @@ export const Grid = () => {
       <input
         class="search"
         onChange={onChange}
-        placeholder="Type here to search..."
+        placeholder="Type here to search projects..."
       />
-
       {
       loading?
        
@@ -32,14 +33,20 @@ export const Grid = () => {
       :
       <div class="cardgrid">
         {
+        
         filtredItems.map((p) => (
+          <a href={p.link} target="_blank" rel="noreferrer">
           <div className="card">
-            <img className="card-img" src={p.src} alt="card" />
+            <img 
+            className="cardimg" src={p.src} alt="card" />
+            
             <div className="card-info">
               <div className="card-title">{p.title}</div>
             </div>
           </div>
+          </a>
         ))}
+      
       </div>
 }
     </div>
--- src/components/data.js
+++ src/components/data.js
@@ -1,40 +1,68 @@
 import p1 from './p1.png'
+import p2 from './p2.png'
+import p3 from './p3.png'
+import p4 from './p4.png'
+import p5 from './p5.png'
+import p6 from './p6.png'
+import p7 from './p7.png'
+import p6j from './p6.jpg'
 const Projects = [
     {
       id: 1,
       src: p1,
-      title: 'room-design-landing-page',
-      link:'room-design-landing-page.vercel.app',
-      description:'this is a challenge from frontend mentor to make website from design i used react library for this website '
+      title: 'Room-landing-page',
+      link:'https://room-design-landing-page.vercel.app',
+      description:'this is a challenge from frontend mentor to make website from design, i used react library for this website '
     },
     {
         id: 2,
-        src: p1,
-        title: 'b',
-        link:'room-design-landing-page.vercel.app',
-        description:'this is a challenge from frontend mentor to make website from design i used react library for this website '
+        src: p2,
+        title: 'News-homepage',
+        link:'https://ouadjih.github.io/Frontend-Mentor---News-homepage/',
+        description:'this is a challenge from frontend mentor to make website from design, i used react library for this website '
       },
       {
         id: 3,
-        src: p1,
-        title: 'c',
-        link:'room-design-landing-page.vercel.app',
-        description:'this is a challenge from frontend mentor to make website from design i used react library for this website '
+        src: p3,
+        title: 'Ecommerce-product-page',
+        link:'https://e-commerce-product-page-ten-jet.vercel.app/',
+        description:'this is an ecommerce page that i made from design, i used react library for this website '
       },
       {
-        id: 4,
-        src: p1,
-        title: 'd',
-        link:'room-design-landing-page.vercel.app',
-        description:'this is a challenge from frontend mentor to make website from design i used react library for this website '
+        id: 7,
+        src: p5,
+        title: 'Menu-search',
+        link:'https://menu-nine-psi.vercel.app/',
+        description:'in this project i created a automatic menu display depending on the category ofthe food  plus responsive design for mobile'
+      },
+      {
+        id: 6,
+        src: p6,
+        title: 'Hotel-reservation-app',
+        link:'https://hotel21.vercel.app/',
+        description:'this is a reservation app for hotel'
       },
       {
         id: 5,
-        src: p1,
-        title: 'room-design-landing-page',
-        link:'room-design-landing-page.vercel.app',
-        description:'this is a challenge from frontend mentor to make website from design i used react library for this website '
-      }
+        src: p6j,
+        title: 'Tenzies-game',
+        link:'https://github.com/ouadjih/Tenzies',
+        description:'this is a tenzie game thati made using react library'
+      },
+      {
+        id: 4,
+        src: p4,
+        title: 'Delivery-app',
+        link:'https://github.com/ouadjih/restaurent-app',
+        description:'restaurent landing page'
+      },
+      {
+        id: 8,
+        src: p7,
+        title: 'Book-reservation-app',
+        link:'https://github.com/ouadjih/bibliotheque',
+        description:'this is a reservation app for books, i used j2ee (jstl for backend and mysql db)'
+      },
    
 ]
 export default Projects;
