33784b06379bca4ac589ef74e872ef65e622d3da
ouadjih <47463757+ouadjih@users.noreply.github.com>
2022-12-28 15:14:45 +0100

    added responsive sidebar for mobile version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,13 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../ouadjih.png";
+import open from "../Assets/Images/open.png";
+import cl from "../Assets/Images/close.png";
 export const Navbar = () => {
   const [screenWidth, getScreenWidth] = useState({
     dynamicWidth: window.innerWidth,
   });
+  const [close, setClose] = useState(false);
   const setScreenWidth = () => {
     getScreenWidth({
       dynamicWidth: window.innerWidth,
@@ -24,7 +27,8 @@ export const Navbar = () => {
       <Link to="/">
         <img class="navbar-logo" src={logo} alt="logo" />
       </Link>
-      {screenWidth.dynamicWidth > 800 ? (
+      {
+      screenWidth.dynamicWidth > 800 ? 
         <ul className="navbar-list">
           <li className="navbar-list-item hvr-buzz">
             <Link to="/Projects">Projects</Link>
@@ -36,19 +40,43 @@ export const Navbar = () => {
             <Link to="/Learn">Learn with me</Link>
           </li>
         </ul>
-      ) : (
+      : 
+      
+      
+      close && screenWidth.dynamicWidth < 800  ? 
+       
         <ul className="navbar-list-phone">
-          <li className="navbar-list-item ">
-            <Link to="/Projects">Projects</Link>
+            <img
+          className="HamburgerCl"
+          onClick={() => setClose(!close)}
+          src={cl}
+          alt="hamburger"
+         />
+         <div className="list-phone">
+          <li className="navbar-list-item-phone">
+            <Link className="Itemph" to="/Projects">
+              Projects
+            </Link>
           </li>
-          <li className="navbar-list-item ">
-            <Link to="/Contact">Contact</Link>
+          <li className="navbar-list-item-phone">
+            <Link className="Itemph" to="/Contact">
+              Contact
+            </Link>
           </li>
-          <li className="navbar-list-item">
-            <Link to="/Learn">Learn with me</Link>
+          <li className="navbar-list-item-phone">
+            <Link className="Itemph" to="/Learn">
+              Learn with me
+            </Link>
           </li>
+          </div>
         </ul>
-      )}
+      : <img
+      className="Hamburger"
+      onClick={() => setClose(!close)}
+      src={open}
+      alt="hamburger"
+     />}
+      
     </nav>
   );
 };
