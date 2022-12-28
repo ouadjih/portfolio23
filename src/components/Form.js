b36fc6802314e6bb20e518c1953a75af08b0e9ea
ouadjih <47463757+ouadjih@users.noreply.github.com>
2022-12-28 18:30:39 +0100

    added emailJS to contact form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,45 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { send } from 'emailjs-com'
 import './Form.css'
+
 export const Form = () => {
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: 'ouadjih',
+    reply_to: '',
+    message: '',
+  });
+  const onSubmit = (e) => {
+   e.preventDefault()
+    send(
+      'service_5pu05q8',
+      'template_4ifqtjc',
+      toSend,
+      'User ID'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
   return (
-    <form className='form'>
+    <form className='form' onSubmit={onSubmit}>
         <label>Full Name*</label>
-        <input className='name' name='name' type="text" placeholder='Full name'/>
+        <input className='name' name='from_name' type="text" placeholder='Full name'  value={toSend.from_name}
+    onChange={handleChange}/>
         <label>Email*</label>
-        <input className='email' name='email' type="email" placeholder='example@mail.com'/>
+        <input className='email' name='reply_to' type="email" placeholder='example@mail.com'  value={toSend.reply_to}
+    onChange={handleChange}/>
         <label>Message*</label>
-        <textarea className='message' name='message' rows="4" cols="50"></textarea>
-        <button className='submit'>Submit</button>
+        <textarea className='message' name='message' rows="4" cols="50"  value={toSend.message}
+    onChange={handleChange}/>
+        <button type="submit" className='submit'>Submit</button>
     </form>
   )
 }
