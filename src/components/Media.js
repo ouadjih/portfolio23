6dd4d4753d42c722ab0cf3fc2161fc7241970c3b
ouadjih <ouadjihboudraa@yahoo.com>
2023-01-04 18:23:59 +0100

    fixed media icons and links

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Media.css'
-import email from '../Assets/Images/email.png'
+import instagram from '../Assets/Images/instagram.png'
 import fb from '../Assets/Images/fb.png'
 import github from '../Assets/Images/github.png'
 import linkedin from '../Assets/Images/linkedin.png'
@@ -15,10 +15,10 @@ export const Media = () => {
       </div>
       
       <div className='medias-contact'>
-        <img src={email} alt="ouadjihboudraa@yahoo.com" className='mediael'/>
-        <a href="www.github.com/ouadjih"> <img src={github} alt="github/ouadjih" className='mediael' /></a>
-        <a href="www.fb.com/ouadjih"> <img src={fb} alt="fb/ouadjih"  className='mediael'/></a>
-        <a href="www.linkedin.com/ouadjih"> <img src={linkedin} alt="linked/ouadjih" className='mediael'/></a>
+        <a  href="https://www.github.com/ouadjih"> <img src={github} alt="github/ouadjih" className='mediael' /></a>
+        <a  href="https://www.fb.com/ouadjih"> <img src={fb} alt="fb/ouadjih"  className='mediael'/></a>
+        <a  href="https://www.linkedin.com/in/ouadjih-boudraa-b8618311a"> <img src={linkedin} alt="linked/ouadjih" className='mediael'/></a>
+        <a  href="https://www.instagram.com/ouadjih/"> <img src={instagram} alt="ig/ouadjih" className='mediael'/></a>
       </div>
     </div>
   )
