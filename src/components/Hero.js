080d0386b69c023a2c099448e91049e24d0d3ade
ouadjih <47463757+ouadjih@users.noreply.github.com>
2023-02-16 13:42:41 +0100

    added background hero & animation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 import './Hero.css'
+import about from '../Assets/Images/about.jpg'
+import {useTheme} from '../context/ThemeContext';
 //import {resume} from ''
 export const Hero = () => {
-  
+  const darktheme = useTheme();
   return (
     <>
     <div className={"hero"}>
   
       <h1>Hello, my name is Ouadjih.</h1>
       <p class="subtitle"><br/>It's all about consistency</p>
+      {darktheme && <img className="img-hero" src={about} alt="about"/>}
       <a className="button-74" target="_blank" href="./ouadjihboudraa.pdf">Resume</a>
     </div>
     </>
